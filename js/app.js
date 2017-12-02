839d72a271c8922b615d33a947554020484f3947
Adam Elders <Adam Elders>
2017-12-02 13:25:08 -0500

    Correcting a few issues
     - Some minor code corrections.
     - Changed creation date shown on notes from today's date to a set value, updated these values (in ms).
     - Fixed warnings with HTML validation related to the directive and controller bindings.
     - Corrected the templateUrl of the noteTemplate directive.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
   
   app.controller('NotesController', ['$sce', function ($sce) {
     this.notes = savedNotes;
-    this.createdOn = Date.now();
     
   }]);
   
@@ -11,24 +10,26 @@
     return {
       restrict: "E",
 
-      templateUrl: "../note-template.html"
+      templateUrl: "note-template.html"
     };
   });
   
   /* Custom filter to display HTML without sanitization */
   app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
   
+  /* Temporary object to store notes data, will factor this out to a JSON file */
+  /* later so we can store notes in */
   savedNotes = [
     {
       title: "ToDo List",
-      content: "1. Learn JavaScript<br><br>2. Learn AngularJS",
-      createdOn: 1397490980837
+      content: "1. Learn JavaScript<br><br>2. Learn AngularJS<br><br>3. Create AngularJS Example!",
+      createdOn: 1512104400000 /* 12/01/2017 */
     },
 
     {
       title: "Test Title",
       content: "This is some test content.<br><br>This is some more <em>test content</em>.",
-      createdOn: 1397490980837
+      createdOn: 1512190800000 /* 12/02/2017 */
     }
   ];
   
