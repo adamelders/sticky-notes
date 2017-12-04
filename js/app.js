e608d5d6a4357c2ac64d050deaf5311ad1ed869f
Adam Elders <ramor.1990@gmail.com>
2017-12-04 13:56:10 -0500

    Create form:
     - Implemented functionality (creating/saving a note).
     - The current notes are now blurred when the Create form is displayed, instead of hidden.
     - The Create form is now always centered on the screen.
     - Implemented the Cancel button on the Create form.
    
    TODO:
    
     - Create a custom alert window for note input validation.
     - Add support for saving the notes to a JSON file, either locally or from an API.

--- js/app.js
+++ js/app.js
@@ -4,14 +4,20 @@
   
   app.controller('NotesController', function() {
     this.notes = savedNotes;
-    this.notesHidden = false;
     this.createFormHidden = true;
     
+    // Blur the notes in the background if the Create form is shown.
     this.hideNotes = function(value) {
-      if (value === true)
-        this.notesHidden = true;
-      else if (value === false)
-        this.notesHidden = false;
+      if (value === true) {
+        var elements = document.getElementsByName("noteContainer");
+        for (var i = 0; i < elements.length; i++)
+          elements[i].style.webkitFilter = "blur(3px)";
+      }
+      else if (value === false) {
+        var elements = document.getElementsByName("noteContainer");
+        for (var i = 0, len = elements.length; i < len; i++)
+          elements[i].style.webkitFilter = "blur(0)";
+      }
     };
     
     this.hideCreateForm = function(value) {
@@ -20,6 +26,46 @@
       if (value === false)
         this.createFormHidden = false;
     };
+  });
+  
+  app.controller('CreateNoteController', function() {
+    
+    // Create an empty object to store note data.
+    this.note = {};
+    
+    // Function to cancel creating a note, hide the create form, and
+    // show the notes. Sets the current note to a new, empty object.
+    this.cancelNote = function(notes) {
+      notes.hideCreateForm(true); 
+      notes.hideNotes(false);
+      this.note = {};
+    }
+    
+    // Adds a new note to the NotesController.notes object. This
+    // will be changed to a JSON file later, so that notes are saved between sessions.
+    this.addNote = function(notes) {
+      
+      // Crude validation, need to create a custom alert.
+      if (this.note.title === undefined) {
+        alert("Your note needs a title!");
+        return;
+      }
+      
+      if (this.note.content === undefined) {
+        alert("Your note needs content!");
+        return;
+      }
+      
+      // Set the creation date for the note, and add the currently saved
+      // notes data to the current scope's note object. Set the new note
+      // to a blank, empty object to clear the fields and data.
+      // Finally, hide the create form and show the notes.
+      this.note.createdOn = Date.now();
+      notes.notes.push(this.note);
+      this.note = {};
+      notes.hideNotes(false);
+      notes.hideCreateForm(true);
+    }
   });
   
   app.directive("noteTemplate", function() {
